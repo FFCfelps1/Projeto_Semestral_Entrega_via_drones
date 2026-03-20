a9192c8f45c3c7a42491fad4aafb8efd4b5b3e70
Felipe Fazio <felipefazio.costa@gmail.com>
2026-03-20 09:54:12 -0300

    feat: Integrate TopBar into App component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,32 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Pedido from "./Pedido.jsx"
+import Cartao from "./Cartao.jsx"
+import Feedback from "./Feedback.jsx"
+import TopBar from "./TopBar.jsx"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+
+  const textoOK = 'Já recebi'
+  const textoNOK = 'Ainda não recebi'
+  const funcaoOK = () => alert("Agradecemos o feedback")
+  const funcaoNOK = () => alert("Verificamos")
+  const componenteFeedBack = (
+    <Feedback 
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+      textoNOK={textoNOK}
+      textoOK={textoOK}
+    />
+  )
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <TopBar />
+      <div className="container border">
+      <div className="row">
+
+        <div className="col-12">
+          {/* i.fa-solid.fa-hippo */}
+          <i className="fa-solid fa-hippo fa-2x"></i>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="row">
+        <div className="col-sm-12 col-md-6 col-xl-3">
+          <Cartao
+          cabecalho="22/03/2025">
+          <Pedido 
+            data="22/02/2026"
+            icone="camera"
+            titulo="Câmera"
+            descricao="Uma câmera 4K"/>
+            {componenteFeedBack}
+            </Cartao>
+        </div>
+        <div className="col-sm-12 col-md-6 col-xl-3">
+          <Cartao
+          cabecalho="18/03/2025">
+          <Pedido 
+            data="20/02/2026"
+            icone="bicycle"
+            titulo="Bicicleta"
+            descricao="Uma bicicleta nova"/>
+            {componenteFeedBack}
+          </Cartao>
+        </div>
+        <div className="col-sm-12 col-md-6 col-xl-3">
+          <Cartao
+          cabecalho="16/02/2026">
+          <Pedido
+            data="18/02/2026"
+            icone="hippo"
+            titulo="Hipopotaamo"
+            descricao="Uma hipopotamo"/>
+            {componenteFeedBack}
+          </Cartao>
+        </div>
+        <div className="col-sm-12 col-md-6 col-xl-3">
+          <Cartao
+          cabecalho="16/05/2025">
+          <Pedido
+            data="16/02/2026"
+            icone="pencil"
+            titulo="Lápis"
+            descricao="Lápis novo"/>
+            {componenteFeedBack}
+          </Cartao>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
