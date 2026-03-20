2f3198a075e90d60bda1f367c33bef9befaa0c1c
Felipe Fazio <felipefazio.costa@gmail.com>
2026-03-20 10:54:16 -0300

    style: Update icon styling in Pedido component to match Advantages section

--- front/src/Pedido.jsx
+++ front/src/Pedido.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Pedido = (props) => {
 	return (
-			<div className="d-flex">
-				<div className="d-flex align-items-center">
-					<i className={`fa fa-${props.icone} fa-2x`}></i>
+			<div className="d-flex gap-3">
+				<div className="d-flex align-items-center justify-content-center flex-shrink-0" style={{ width: '60px', height: '60px', backgroundColor: '#e7f3ff', borderRadius: '8px' }}>
+					<i className={`fa fa-${props.icone} fa-xl text-primary`}></i>
 				</div>
 				{/* div>(h4.text-center+p.text-center) */}
 				<div className="flex-grow-1 rounded p-3">
