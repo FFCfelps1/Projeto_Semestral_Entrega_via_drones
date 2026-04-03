23a72116a193bd83c18845bea7f5c568358a8964
Arthur Ruiz <23.01445-8@maua.br>
2026-04-03 12:36:31 -0300

    feat(endpoint): cria endpoint para calcular rota

--- back/entrega_via_drone/server.js
+++ back/entrega_via_drone/server.js
@@ -7,3 +7,23 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Cria um endpoint para calcular rota
+app.get('/rota', async (req, res) => {
+  const { origemLat, origemLng, destinoLat, destinoLng } = req.query;
+
+  try {
+    const url = `https://router.openstreetmap.de/route/v1/driving/${origemLng},${origemLat};${destinoLng},${destinoLat}?overview=full&geometries=geojson`;
+
+    const response = await axios.get(url);
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ erro: 'Erro ao calcular rota' });
+  }
+});
+
+app.listen(3002, () => {
+  console.log('Map service rodando na porta 3002');
+});
