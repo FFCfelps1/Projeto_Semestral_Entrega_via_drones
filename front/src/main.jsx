9849f12b3e46a93ea06c2d36353cc96c6670d888
Arthur Ruiz <23.01445-8@maua.br>
2026-04-03 13:57:46 -0300

    build: importacao de leaflet

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-
+import 'leaflet/dist/leaflet.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
