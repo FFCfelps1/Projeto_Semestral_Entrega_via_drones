9cf48bbefa717e90e49897489d27806c01a25c8b
leoolivieri <olivierileo30@gmail.com>
2026-05-19 22:28:43 -0300

    feat(gestao_pedidos): adiciona rota POST /pedidos

--- back/gestao_de_pedidos/server.js
+++ back/gestao_de_pedidos/server.js
@@ -24,6 +24,33 @@ const STATUS = {
 };
 
 // ─────────────────────────────────────────
+// CRIAR PEDIDO
+// ─────────────────────────────────────────
+app.post("/pedidos", (req, res) => {
+  const { item, peso, origem, destino, tipo, observacoes, usuarioId } = req.body;
+
+  const novoPedido = {
+    id: uuidv4(),
+    item,
+    peso,
+    origem,
+    destino,
+    tipo,
+    observacoes: observacoes || "",
+    usuarioId: usuarioId || null,
+    status: STATUS.RASCUNHO,
+    /*new Date().toISOString() — gera a data/hora atual
+    22:25 O new Date() cria um objeto com a data e hora atual. O .toISOString() converte esse objeto para uma string no formato padrão internacional:
+    2026-05-19T22:35:10.123Z*/
+    criadoEm: new Date().toISOString(),
+    atualizadoEm: new Date().toISOString(),
+  };
+
+  pedidos.push(novoPedido);
+
+  return res.status(201).json(novoPedido);
+});
+// ─────────────────────────────────────────
 // HEALTH CHECK
 // ─────────────────────────────────────────
 app.get("/health", (req, res) => {
